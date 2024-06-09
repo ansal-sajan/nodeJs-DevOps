77050d9db8944b00e66e3d36b070f375411d59a9
Ansal Sajan <ansalsajan.eee@gmail.com>
2024-06-09 10:01:14 +0000

    Added

---
+++ ecs-fargate-terraform/main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+
+resource "aws_ecs_cluster" "main" {
+  name = "ecs-fargate-cluster"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ecs_task_execution_role_policy" {
+  name       = "ecsTaskExecutionRolePolicyAttachment"
+  roles      = [aws_iam_role.ecs_task_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+resource "aws_ecs_task_definition" "my_app" {
+  family                   = "my-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"  # 0.25 vCPU
+  memory                   = "512"  # 512 MB
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "webserver"
+    image     = "nginx:latest"  # Replace with your container image
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+      protocol      = "tcp"
+    }]
+  }])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+}
+
+resource "aws_ecs_service" "my_app_service" {
+  name            = "my-app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.my_app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-0116beff52605851f"]  # Specify your public subnet ID here
+    security_groups = ["sg-04b1422f9d03e2e3b"]
+    assign_public_ip = true
+  }
+
+
+  tags = {
+    Name = "my-app-service"
+  }
+}
+
